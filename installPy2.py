704dd76f0fa57122f4958f3e1d536cf14b75f0e4
TysonMcLaws <47451713+TysonMcLaws@users.noreply.github.com>
2019-11-19 09:12:30 -0600

    Update installPy2.py

--- installPy2.py
+++ installPy2.py
@@ -1,4 +1,4 @@
 f = open(".bashrc","a")
-f.write("alias killComputer='python3 killcomputer.py\r\n'")
-f.write("alias killComputer2='python2 killcomputer2.py\r\n'")
+f.write("alias killComputer='python3 killcomputer.py'\n")
+f.write("alias killComputer2='python2 killcomputer2.py'\n")
 f.close()
