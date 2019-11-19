90d5f5a31b20befa1f0ce858c531070a18685c48
TysonMcLaws <47451713+TysonMcLaws@users.noreply.github.com>
2019-11-19 13:08:58 -0600

    Update comDeathInstall.sh
    Removed unnecessary pip installs (now only works for Python3)

--- comDeathInstall.sh
+++ comDeathInstall.sh
@@ -12,26 +12,17 @@ then
   pip3 install colorama
   pip3 install datetime
   pip3 install time
-  pip install os
-  pip install colorama
-  pip install datetime
-  pip install time
-  pip2 install os
-  pip2 install colorama
-  pip2 install datetime
-  pip2 install time
   
   wget https://raw.githubusercontent.com/TysonMcLaws/computerDeath/master/computerDeath.py
   wget https://raw.githubusercontent.com/TysonMcLaws/computerDeath/master/computerDeath2.py
   wget https://raw.githubusercontent.com/TysonMcLaws/computerDeath/master/installPy2.py
   
   python3 installPy2.py
-  python2 installPy2.py
   rm -r installPy2.py
   source .bashrc
   
   clear
-  echo -e "\nIf you are running Python3 use \"killComputer\" to start the program.  If you are using Python2 use \"KillComputer2\"\n"
+  echo -e "\nIf you are running Python3 use \"killComputer\" to start the program.\n"
 else
   echo ":( :( Saddness, goodbye"
   clear
